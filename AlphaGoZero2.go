d626a19a5d1c132087accd620bd2e3d331494a61
Ryan <cumt.xiaochi@gmail.com>
2017-11-04 11:25:57 +0800

    go fmt all files

--- AlphaGoZero2.go
+++ AlphaGoZero2.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-  // "fmt"
-  // "errors"
-  "log"
-  )
+	// "fmt"
+	// "errors"
+	"log"
+)
 
 // 棋盘大小
 const BOARD_SIZE = 19
+
 // 棋盘数据
 // 0 无棋子
 // 1 黑子
@@ -15,22 +16,25 @@ const BOARD_SIZE = 19
 // Golang will init all value to zero
 // https://golang.org/ref/spec#Variable_declarations
 type go_color byte
-var go_data [BOARD_SIZE][BOARD_SIZE] go_color
+
+var go_data [BOARD_SIZE][BOARD_SIZE]go_color
+
 const (
-  NONE  = 0
-  BLACK = 1
-  WHITE = 2
+	NONE  = 0
+	BLACK = 1
+	WHITE = 2
 )
-var go_color_repr_map = [3]byte {'.', 'x', 'o'}
+
+var go_color_repr_map = [3]byte{'.', 'x', 'o'}
 
 func main() {
-    var err error
-    if err = one_move_(1,3,WHITE); err != nil {
-      log.Fatal(err)
-    }
-    print_go_board()
+	var err error
+	if err = one_move_(1, 3, WHITE); err != nil {
+		log.Fatal(err)
+	}
+	print_go_board()
 
-    qz_neibour := [...]*qizi{nil,nil,nil,nil}
-    qz := qizi{2,9,10,qz_neibour}
-    go_die_qi_print_iter(&qz,0)
+	qz_neibour := [...]*qizi{nil, nil, nil, nil}
+	qz := qizi{2, 9, 10, qz_neibour}
+	go_die_qi_print_iter(&qz, 0)
 }
--- go_die.go
+++ go_die.go
@@ -3,17 +3,17 @@
 package main
 
 import (
-  "fmt"
-  "strings"
-  // "errors"
-  // "log"
+	"fmt"
+	"strings"
+	// "errors"
+	// "log"
 )
 
 // 棋子和他的四个邻居们
 type qizi struct {
 	color go_color
-	i int
-  j int
+	i     int
+	j     int
 	// 0 1 2 3
 	//   0
 	// 1 o 2
@@ -23,21 +23,21 @@ type qizi struct {
 
 // 打印这个结构（辅助调试）
 func go_die_qi_print_tab(tab int) {
-		fmt.Printf("%s", strings.Repeat("\t",tab));
+	fmt.Printf("%s", strings.Repeat("\t", tab))
 }
 func go_die_qi_print_iter(qz *qizi, tab int) {
 	if qz != nil {
 		fmt.Printf("(%d,%d) %c\n",
 			qz.i, qz.j,
-			go_color_repr_map[qz.color]);
+			go_color_repr_map[qz.color])
 		for i := 0; i < 4; i++ {
-			go_die_qi_print_tab(tab+1);
-			fmt.Printf("| ");
-			go_die_qi_print_iter(qz.neibour[i], tab+1);
+			go_die_qi_print_tab(tab + 1)
+			fmt.Printf("| ")
+			go_die_qi_print_iter(qz.neibour[i], tab+1)
 		}
-		fmt.Printf("\n");
+		fmt.Printf("\n")
 	} else {
-		fmt.Printf("nil\n");
+		fmt.Printf("nil\n")
 	}
 }
 
