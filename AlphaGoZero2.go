dfa9e0c77f8ee5d455968297772c30ad93263ae3
Ryan <cumt.xiaochi@gmail.com>
2017-11-04 10:50:28 +0800

    split to files

--- AlphaGoZero2.go
+++ AlphaGoZero2.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-  "fmt"
-  "errors"
+  // "fmt"
+  // "errors"
   "log"
   )
 
@@ -23,44 +23,10 @@ const (
 )
 var go_color_repr_map = [3]byte {'.', 'x', 'o'}
 
-
-// 打印棋盘
-func print_go_board() {
-	for i := 0; i < BOARD_SIZE; i++ {
-		for j := 0; j < BOARD_SIZE; j++ {
-			fmt.Printf("%c ", go_color_repr_map[ go_data [i] [j] ])
-		}
-		fmt.Printf("(%d)\n", BOARD_SIZE-i)
-	}
-	for i := 0; i < BOARD_SIZE; i++  {
-		fmt.Printf("%c ", 'A'+i)
-	}
-	fmt.Printf("\n")
-}
-
-func assert(cond bool) {
-  if !cond {
-    log.Fatal(cond)
-  }
-}
-
-// 在某处位置走一步棋(计算机版本)
-func one_move_(i int, j int, color go_color) (err error) {
-	assert(i >= 0 && i < BOARD_SIZE)
-	assert(j >= 0 && j < BOARD_SIZE)
-	if go_data [i] [j] != NONE {
-    return errors.New("can not move on an already point")
-  }
-	go_data[i][j] = color
-	return nil
-}
-
 func main() {
-    go_data[1][2] = BLACK
     var err error
     if err = one_move_(1,3,WHITE); err != nil {
       log.Fatal(err)
     }
     print_go_board()
-
 }
---
+++ go_lib.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+  "fmt"
+  "errors"
+  "log"
+  )
+
+// 打印棋盘
+func print_go_board() {
+	for i := 0; i < BOARD_SIZE; i++ {
+		for j := 0; j < BOARD_SIZE; j++ {
+			fmt.Printf("%c ", go_color_repr_map[ go_data [i] [j] ])
+		}
+		fmt.Printf("(%d)\n", BOARD_SIZE-i)
+	}
+	for i := 0; i < BOARD_SIZE; i++  {
+		fmt.Printf("%c ", 'A'+i)
+	}
+	fmt.Printf("\n")
+}
+
+func assert(cond bool) {
+  if !cond {
+    log.Fatal(cond)
+  }
+}
+
+// 在某处位置走一步棋(计算机版本)
+func one_move_(i int, j int, color go_color) (err error) {
+	assert(i >= 0 && i < BOARD_SIZE)
+	assert(j >= 0 && j < BOARD_SIZE)
+	if go_data [i] [j] != NONE {
+    return errors.New("can not move on an already point")
+  }
+	go_data[i][j] = color
+	return nil
+}
