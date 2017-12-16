f0a603628412b0d220c7508c896cfab0324ddaf4
Ryan <cumt.xiaochi@gmail.com>
2017-12-16 23:12:28 +0800

    play tree

---
+++ go_play_tree_test.go
@@ -0,0 +1,21 @@
+package main
+
+// 数据结构
+import (
+	// "fmt"
+	// "strings"
+	// "errors"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestTree(t *testing.T) {
+	GoOneMove(1, 1, WHITE)
+	f, err := os.OpenFile("test_tree.html", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	GoDrawTree(f)
+}
